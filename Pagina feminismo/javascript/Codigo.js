0d8ba6a1fcef2e03a558b315bcc55ec69758aeca
IronFalconer <82568775+IronFalconer@users.noreply.github.com>
2021-04-15 09:03:52 +0200

    Add files via upload

---
+++ Pagina feminismo/javascript/Codigo.js
@@ -0,0 +1,23 @@
+// "use strict"
+// $(function () {
+//     var cerrado = false;
+//     $(".change").on("click", function () {
+//         if(!cerrado){
+//             cerrado =true;  
+//             console.log("cerrar")    
+//             $("#cambiarImg").attr("src","src/candadoCerrado.png");
+//         }
+//         else{
+//             cerrado=false;
+//             console.log("abrir")    
+//             $("#cambiarImg").attr("src","src/candadoAbierto.png");
+//         }
+//     });
+
+//     $("#nombre").on("change", function () {
+//         let name=$("#nombre").val();
+//         console.log(name);
+//         $("#ponerNombre").text(name)
+//     });
+    
+// }); 
