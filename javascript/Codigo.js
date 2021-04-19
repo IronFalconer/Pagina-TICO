8b36ceb8b386ddc445237d32108d1bec84428ae8
IronFalconer <82568775+IronFalconer@users.noreply.github.com>
2021-04-19 17:27:01 +0200

    Add files via upload

---
+++ javascript/Codigo.js
@@ -0,0 +1,11 @@
+// primer conjunto de variables
+var x = "false"
+let x = {a="true",b="false",c="false"}
+
+// segundo conjunto de variables
+var y = {d="false",f="true",g="false"}
+
+// tercer conjunto de variables
+var z = {h="false",i="false",j="true"}
+
+
